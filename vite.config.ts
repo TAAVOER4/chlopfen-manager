e6e9b5f0893554c0b531fee5311bf507c9cee144
Erwin Vogel <91468537+TAAVOER4@users.noreply.github.com>
2025-04-01 10:50:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["09e46c31-5eef-4be1-9188-8d623483dd3a.lovableproject.com"]
   },
   plugins: [
     react(),
